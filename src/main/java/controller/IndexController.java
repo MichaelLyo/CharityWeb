f154464bb1e53db810912213dcec15a3d16e5266
511231530@qq.com <lsl@liushuliangdeMacBook-Pro.local>
2017-11-13 22:46:06 +0800

    remove spring boot components from the project

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,26 +1,35 @@
 package controller;
 
+import org.apache.commons.collections4.Get;
+import org.omg.CORBA.Request;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by LSL on 2017/11/13
  */
 
-//@Controller    // This means that this class is a Controller
-@RestController
+@Controller    // This means that this class is a Controller
 @RequestMapping(path="/") // This means URL's start with / (after Application path)
 public class IndexController
 {
-	@RequestMapping("/index")
-	public String index()
+	@RequestMapping(value = "/index",method = RequestMethod.GET)
+	public ModelAndView index()
 	{
-		return "index";
+		return new ModelAndView("index");
 	}
 
 	@RequestMapping("/login")
-	public String login()
+	public ModelAndView login()
 	{
-		return "login";
+		return new ModelAndView("login");
+	}
+	@RequestMapping("/test")
+	public String test()
+	{
+		return "test";
 	}
 }
---
+++ src/main/webapp/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lsl
+  Date: 2017/11/13
+  Time: 下午9:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>test</title>
+</head>
+<body>
+mytest
+</body>
+</html>
