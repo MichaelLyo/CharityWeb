6599bdae062274b30db50c95dfd704626ddfbe01
Jonathan <838455960@qq.com>
2017-12-24 14:59:24 +0800

    add ajax 优化mgtprojects

--- src/main/java/com/tongji/charityweb/controller/ProjectController.java
+++ src/main/java/com/tongji/charityweb/controller/ProjectController.java
@@ -13,8 +13,11 @@ import com.tongji.charityweb.repository.project.ParRepository;
 import com.tongji.charityweb.repository.project.ProFolRepository;
 import com.tongji.charityweb.repository.project.ProjectRepository;
 import com.tongji.charityweb.repository.repository.RepRepository;
+import com.tongji.charityweb.repository.user.UserRepository;
+import com.tongji.charityweb.service.FileService;
 import com.tongji.charityweb.service.ProjectService;
 import com.tongji.charityweb.service.UserService;
+import net.minidev.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.data.jpa.convert.threetenbp.ThreeTenBackPortJpaConverters;
@@ -24,13 +27,11 @@ import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.session.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import redis.clients.jedis.BinaryClient;
+import sun.security.util.Length;
 import sun.util.resources.CalendarData;
 
 import javax.jws.WebParam;
@@ -39,10 +40,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class ProjectController {
@@ -58,11 +56,50 @@ public class ProjectController {
     ProFolRepository proFolRepository;
     @Autowired
     ParRepository parRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    FileService fileService;
 
     @RequestMapping(value = "createProject" ,method = RequestMethod.POST)
-    public String getCreateProjectPage(HttpServletRequest request){
+    public String getCreateProjectPage(HttpServletRequest request, Model model){
+        String repName = request.getParameter("repName");
+        model.addAttribute("repName",repName);
+
         return "management/addProject";
     }
+
+    @RequestMapping(value = "testAjax",  method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,Object> testAjax(String userName, String repName, String projName){
+        System.out.println("hello");
+        Map<String,Object> resultMap = new HashMap<String, Object>();
+        List<Project> projects = new ArrayList<>() ;
+        Project project = projectRepository.findOne(new ProjectID(projName, repName, userName));
+        projects.add(project);
+        resultMap.put("projects", projects);
+        System.out.println(userName);
+        System.out.println(repName);
+        System.out.println(projName);
+        Map map = new HashMap();
+       if(project.getFollowers() == null)
+           map.put("followerNum",0);
+       else
+           map.put("followerNum",project.getFollowers().size());
+
+       if(project.getParticipates()==null)
+           map.put("participateNum",0);
+       else
+           map.put("participateNum",project.getParticipates().size());
+
+       map.put("projName",projName);
+       map.put("context",project.getContext());
+       map.put("endDate",project.getEndDate().toString().replace("00:00:00.0",""));
+
+
+        return map;
+    }
+
 
     @RequestMapping(value = "searchProject", method = RequestMethod.POST)
     public String searchProject(HttpServletRequest request, Model model){
@@ -86,61 +123,66 @@ public class ProjectController {
             return "error";
         }
     }
-
-    @RequestMapping(value = "examCreateProject", method = {RequestMethod.POST})
-    public String createProject(MultipartHttpServletRequest request, Model model){
+    @RequestMapping(value = "searchWithUserNameProject", method = RequestMethod.POST)
+    public String searchWithUserNameProject(HttpServletRequest request, Model model){
         try{
-            System.out.println("enheng");
+            String projName = request.getParameter("projName");
+            String userName =request.getParameter("userName");
+            List<Project> projects = new ArrayList<>() ;
+
+            User user = userRepository.findByUsername(userName);
+            projects = projectRepository.findByUserNameAndProjName(userName, projName);
+            model.addAttribute("projects", projects);
+            model.addAttribute("pictureUrl",user.getHpPictureUrl());
+            return "management/showOwnerProjects";
+        }
+        catch (Exception e){
+            System.out.println("??");
+            return "error";
+        }
+    }
+    @RequestMapping(value = "examCreateProject", method = {RequestMethod.POST})
+    public String createProject(MultipartHttpServletRequest request, Model model,MultipartHttpServletRequest mulRequest){
+        try{
             User user = userService.getUserInSession(request.getSession());
             if(user==null)
                 return "login/sessionLost";
             HttpSession session = request.getSession();
-            String repName = session.getAttribute("curRep").toString();
-            String userName = session.getAttribute("curUser").toString();
+            String repName = request.getParameter("repName");
             String projName = request.getParameter("projName");
             String context = request.getParameter("context");
             String startDate = request.getParameter("startDate");
             String endDate = request.getParameter("endDate");
+            MultipartFile file = mulRequest.getFile("photo");
 
-            Project project = new Project(repName, projName, userName );
+            Project project = new Project(repName, projName, user.getUsername() );
             project.setContext(context);
             DateFormat fmt =new SimpleDateFormat("yyyy-MM-dd");
             Date date = fmt.parse(endDate);
             project.setEndDate(date);
-
+            String projectUrl = fileService.uploadProjectPicture(file, project);
+            project.setDescriptionPictureUrl(projectUrl);
             Repository repository = repRepository.findOne(new RepositoryID(user.getUsername(), repName));
             repository.addProject(project);
             repRepository.save(repository);
-            return "redirect:/afterCreateProject";
-        }
-        catch (Exception e){
-            return "error";
-        }
 
-    }
-
-    @RequestMapping(value = "afterCreateProject", method = RequestMethod.GET)
-    public String afterCreateProject(HttpServletRequest request, Model model)
-    {
-        try{
-            HttpSession session = request.getSession();
-            String repName = session.getAttribute("curRep").toString();
-            String userName = session.getAttribute("curUser").toString();
-            Repository repository = repRepository.findOne(new RepositoryID(userName, repName));
             List<Project> projects = repository.getProjects();
-
             String pictureUrl = repository.getDescriptionPictureUrl();
             String pictureName = repository.getRepositoryName();
             model.addAttribute("projects", projects);
             model.addAttribute("pictureUrl",pictureUrl);
             model.addAttribute("pictureName",pictureName);
+            model.addAttribute("userName",user.getUsername());
+            model.addAttribute("repName",repName);
             return "management/mgtProject";
         }
         catch (Exception e){
-            System.out.println("afterCreateProject");
+            System.out.println("examCreateProject");
             return "error";
         }
+
     }
+
 
     @RequestMapping(value = "showOwnerProjects",method = {RequestMethod.POST, RequestMethod.GET})
     public String showOwnerProjects(HttpServletRequest request, Model model){
@@ -152,14 +194,12 @@ public class ProjectController {
                 return "login/sessionLost";
 
             List<Project> projects = projectRepository.findByUserName(userInsession.getUsername());
-            String pictureUrl = userInsession.getHpPictureUrl();
-            String pictureName = userInsession.getUsername();
             model.addAttribute("projects", projects);
-            model.addAttribute("pictureUrl",pictureUrl);
-            model.addAttribute("pictureName",pictureName);
+            model.addAttribute("pictureUrl",userInsession.getHpPictureUrl());
+            model.addAttribute("pictureName", userInsession.getUsername());
 
 
-            return "management/mgtProject";
+            return "management/showOwnerProjects";
         }
         catch (Exception e){
             System.out.println("showProjects");
@@ -176,15 +216,14 @@ public class ProjectController {
             String repName = request.getParameter("repName");
             String userName = request.getParameter("userName");
 
-            session.setAttribute("curRep",repName);
-            session.setAttribute("curUser",userName);
             Repository repository = repRepository.findOne(new RepositoryID(userName, repName));
             List<Project> projects = repository.getProjects();
 
             model.addAttribute("projects", projects);
             model.addAttribute("pictureUrl",repository.getDescriptionPictureUrl());
             model.addAttribute("pictureName",repName);
-
+            model.addAttribute("userName",userName);
+            model.addAttribute("repName",repName);
 
             return "management/mgtProject";
         }
@@ -195,6 +234,15 @@ public class ProjectController {
 
     }
 
+    @RequestMapping(value = "showProjectDetail",method = RequestMethod.POST)
+    public String showProjectDetail(HttpServletRequest request, Model model){
+        String userName = request.getParameter("userName");
+        String repName = request.getParameter("repName");
+        String projName = request.getParameter("projName");
+        Project project = projectRepository.findOne(new ProjectID(projName, repName, userName));
+        model.addAttribute("project",project);
+        return "action/activity";
+    }
     @RequestMapping(value = "/createProFol",method = RequestMethod.GET)
     public String createProFol(String projName, String repName, String userName, HttpServletRequest request)
     {
