046413bd2517632c0f4b0bcc179135222dcb6c8a
lihang <1154926706@qq.com>
2017-12-26 11:06:35 +0800

    捐款管理页面分页，活动页面捐款总额绑定

--- src/main/java/com/tongji/charityweb/repository/project/ProjectRepository.java
+++ src/main/java/com/tongji/charityweb/repository/project/ProjectRepository.java
@@ -3,18 +3,18 @@ package com.tongji.charityweb.repository.project;
 import com.tongji.charityweb.model.project.Project;
 import com.tongji.charityweb.model.project.ProjectID;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Transactional
 @Repository
 public interface ProjectRepository  extends JpaRepository<Project,ProjectID> {
     public List<Project> findAllByRepNameAndUserName(String repName, String userName);
-    public Page<Project> findAllByRepNameAndUserName(String repName, String userName, org.springframework.data.domain.Pageable pageable);
+    public Page<Project> findAllByRepNameAndUserName(String repName, String userName, Pageable pageable);
     public List<Project> findByUserName(String userName);
     public List<Project> findByProjNameLike(String projName);
 }
