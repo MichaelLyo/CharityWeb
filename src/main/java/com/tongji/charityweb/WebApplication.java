6326a55de70ca23adb7a0f9b6a493eddbfb97923
Jonathan <838455960@qq.com>
2018-01-05 20:53:08 +0800

    按照电影名称查询

--- src/main/java/com/tongji/charityweb/WebApplication.java
+++ src/main/java/com/tongji/charityweb/WebApplication.java
@@ -14,7 +14,6 @@ public class WebApplication
 {
 	public static void main(String[] args)
 	{
-		//System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(WebApplication.class, args);
 	}
 }
